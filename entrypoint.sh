32d9b5e7098f5ff5cddc01ff6be1fc2d1fc9fc79
Siddharth <128633880+sidd-harth-1@users.noreply.github.com>
2023-11-08 20:54:18 +0530

    [skip ci]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Parse GitHub event JSON file
+# Get Token and labels from GitHub Action inputs
 GITHUB_TOKEN=$1
 LABELS=$2
+
+# Parse GitHub event JSON file
 ISSUE_NUMBER=$(jq --raw-output .issue.number "$GITHUB_EVENT_PATH")
 URL=$(jq --raw-output .issue.repository_url "$GITHUB_EVENT_PATH")
-
-echo -------------------------------------------------------------------------------
-echo "$URL/issues/$ISSUE_NUMBER/labels"
-echo -------------------------------------------------------------------------------
-echo $LABELS
-echo -------------------------------------------------------------------------------
-echo "  -d '{\"labels\":$LABELS}'"
-echo -------------------------------------------------------------------------------
 
 
 # Assign a labels to an issue
